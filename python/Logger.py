d92f519efcb18415103ac1ba6baa4a87a300420f
steven <steven@hotmail.com>
2018-09-13 13:32:34 +0800

    logging test

---
+++ python/Logger.py
@@ -0,0 +1,34 @@
+import os
+import logging
+import datetime
+
+class Logger:
+    def __init__(self,
+                 logFile = str(datetime.date.today()).replace("-", ""),
+                 level = logging.DEBUG,
+                 format = '%(asctime)-15s %(name)-12s [%(levelname)-8s] %(message)s',
+                 datefmt = '%Y-%m-%d %H:%M:%S'):
+        if not os.path.isdir("./Log"):
+            os.mkdir("./Log")
+            file = open("./Log/"+logFile + ".log","w",encoding="utf8")
+        if os.getenv("HOSTNAME"):
+            self.name = os.getenv("HOSTNAME")    # linux
+        else:
+            self.name = os.getenv("COMPUTERNAME")    # windows
+        self.level = level
+        self.foramt = format
+        self.datefmt = datefmt
+        self.handlers = [logging.FileHandler('./Log/' + logFile + ".log", 'a', encoding='utf-8'),]
+        logging.basicConfig(level = self.level,
+                            format = self.foramt,
+                            datefmt = self.datefmt,
+                            handlers = self.handlers)
+        self.logger = logging.getLogger(self.name)
+    def setDebug(self,msg):
+        self.logger.debug(msg)
+    def setInfo(self,msg):
+        self.logger.info(msg)
+    def setWarning(self,msg):
+        self.logger.warning(msg)
+    def setError(self,msg):
+        self.logger.error(msg)
